cf672bf1568644c5d7bf39fec3e01fbe105862bb
Taizilinger123 <837337164@qq.com>
2018-12-18 22:15:05 +0800

    2018-12-18

---
+++ src/day05/Homework.java
@@ -0,0 +1,26 @@
+package day05;
+//作业讲解
+public class Homework {
+	public static void main(String[] args) {
+		/*
+		//9+99+999+...+9999999999
+		long sum=0; //和
+		long num=0; //每次加的那个数(9,99,999)
+		for(int i=1;i<=10;i++){
+			num = num*10+9;
+			sum = sum+num;
+		}
+		System.out.println("sum="+sum);
+		*/
+		
+		/*
+		//1+1/2+1/3+...+1/n
+		double sum = 0;
+		int n = 15;
+		for(int i=1;i<=n;i++){
+			sum = sum + 1.0/i;
+		}
+		System.out.println("sum="+sum);
+		*/
+	}
+}
