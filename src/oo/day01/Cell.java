41c91e76a5759318b6361336668c1039704a535d
Taizilinger123 <837337164@qq.com>
2018-12-25 22:44:26 +0800

    2018-12-25

---
+++ src/oo/day01/Cell.java
@@ -0,0 +1,18 @@
+package oo.day01;
+//格子类 
+public class Cell {
+  int row;//行号
+  int col;//列号
+  
+  void drop(){//下落一格
+	  row++;//行号增1
+  }
+  
+  void moveLeft(int n){
+	  col-=n;//列号减n
+  }
+  String getCellInfo(){//获取行号和列号
+	  return col+","+col; //返回行列号
+  }
+  
+}
