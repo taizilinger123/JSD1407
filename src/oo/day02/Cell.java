3212fa2fd59458ae810307ac4cbe21779a0aaaa0
Taizilinger123 <837337164@qq.com>
2018-12-29 23:03:58 +0800

    2018-12-29

--- src/oo/day02/Cell.java
+++ src/oo/day02/Cell.java
@@ -4,6 +4,19 @@ public class Cell {
   int row;//行号
   int col;//列号
   
+  Cell(){
+	  this(0);
+  }
+  
+  Cell(int n){
+	  this(n, n);
+  }
+  
+  Cell(int row, int col){
+	  this.row = row;
+	  this.col = col;
+  }
+  
   void drop(){//下落一格
 	  row++;//行号增1
   }
--- src/oo/day02/CellTest.java
+++ src/oo/day02/CellTest.java
@@ -2,6 +2,12 @@ package oo.day02;
 //格子类的测试类
 public class CellTest {
 	public static void main(String[] args) {
+	  Cell c1 = new Cell();    //调无参构造
+	  Cell c2 = new Cell(3);   //调1个参数构造
+	  Cell c3 = new Cell(2,5); //调2个参数构造
+	  //Cell c4 = new Cell(2,5,8); //编译错误，没有3个参数的构造
+	  
+	  /*	
       Cell c = new Cell();
       c.row = 4;
       c.col = 9;
@@ -11,6 +17,7 @@ public class CellTest {
       c.moveLeft(6);
 	  String str = c.getCellInfo();
 	  System.out.println(str);//8,2
+	  */
 	}
     //打墙+打格
 	public static void printWall(Cell cc){
