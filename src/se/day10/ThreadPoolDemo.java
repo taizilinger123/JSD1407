e514a7e23f2b3df8c854e5040419ab5b14be473d
Taizilinger123 <837337164@qq.com>
2019-06-16 17:34:26 +0800

    2019-6-16

---
+++ src/se/day10/ThreadPoolDemo.java
@@ -0,0 +1,33 @@
+package se.day10;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 线程池
+ * 用于重用线程以及控制线程数量
+ * @author sige
+ *
+ */
+public class ThreadPoolDemo {
+  public static void main(String[] args) {
+	 ExecutorService threadPool = Executors.newFixedThreadPool(2);
+	 for(int i=0;i<5;i++){
+			 Runnable runn = new Runnable() {
+				public void run() {
+					for(int i=0;i<5;i++){
+						System.out.println(i);
+						try {
+							Thread.sleep(1000);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+					
+				}
+			};
+	
+	        threadPool.execute(runn);
+	 }
+  }
+}
