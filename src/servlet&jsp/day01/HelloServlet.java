506aba7ce1778a0e770ddabfd1f025488f5b6456
taizlinger123 <837337164@qq.com>
2020-01-04 22:19:45 +0800

    2020-1-4

---
+++ src/servlet&jsp/day01/HelloServlet.java
@@ -0,0 +1,12 @@
+//package first;
+//import javax.servlet.*;
+//import javax.servlet.http.*;
+//import java.io.*;
+//public class HelloServlet extends HttpServlet{
+//    public void service(HttpServletRequest request, HttpServletResponse response)throws ServletException,IOException{
+//       response.setContentType("text/html");
+//       PrintWriter out = response.getWriter();
+//       out.println("<h1>Hello Kitty</h1>");
+//       out.close();
+//    }
+//}
